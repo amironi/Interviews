49ff2ba8b118f280a193305bdcd9db0d271bfc28
amir <am5050@gmail.com>
2017-11-01 23:17:50 +0200

    .

--- Divedby2.cpp
+++ Divedby2.cpp
@@ -2,13 +2,16 @@
 #include <iosrteam>
 #include <stdio.h>
 
-int array = {1,2,3,4,5,6,7,8,9,10}
+int arr1[] = {1,2,3,4,5,6,7,8,9,10};
+int arr2[] = {10,9,9,8,7,6,5,4,2,3,1,2,3,4,5,6,7,8,9,10};
 
 void OddFunc(int* array, int n)
 {
     for(int i = 1 ; i <n ; ++i)
     {
-        if(i%2)
+        int sign = i%2 ? 1 : -1;
+        
+        if(  sign * (array[i-1] - array[i]) > 0 )
             swap(array[i-1],array[i]);
     }
     
