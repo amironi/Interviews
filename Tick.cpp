a0beb367bc4a7f94f349ec6df78856294a6674e5
amir <am5050@gmail.com>
2018-09-28 14:16:17 +0300

    .

---
+++ Tick.cpp
@@ -0,0 +1,24 @@
+
+
+
+map<tick, list<pair<task, interval>>> interval_2_task_;
+
+uint_64_t tick_;
+
+void AdoTick(interval, task) { 
+  inteval_2_task_[tick_ + interval].push(pair<interval, task>)
+}
+
+void Run() {
+  while(1) {
+    auto front = interval_2_task_.pop();
+
+    sleep(front.first - tick_);
+
+    tick_ = front.first;
+
+    RunAll(front);
+
+    AddTick(front);
+  }
+}
