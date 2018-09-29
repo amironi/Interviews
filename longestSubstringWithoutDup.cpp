e2a4c9fe73d13e03c56741d829b195c5904805d1
amir <am5050@gmail.com>
2018-09-29 16:01:31 +0300

    0.

--- longestSubstringWithoutDup.cpp
+++ longestSubstringWithoutDup.cpp
@@ -1,33 +1,29 @@
 
-string longestSubstringWithoutDup(const string s)
-{
-  string res;
-  string max_res;
-  unsigned int max = 0;
-  
-  unordered_map<char,unsigned int> letters;
-  
-  for(unsigned int i = 0 ; i < s.size(); i++)
-  {
-    if(letters.find(s[i]) == letters.end() )
-    {
-      res +=s[i];
-      
-      if( res.size() > max ) {
-        max_res = res;
-        max = res.size();
-      }
-
-    }
-    else
-    {
-      res = s.substr(letters[s[i]]+1,i-1);
-      
-      cout << res << endl;
-    }
+string longestSubstringWithoutDup(const string s) {
+    string res;
+    string max_res;
+    unsigned int max = 0;
     
-    letters[s[i]] = i;
-  }
+    unordered_map<char,unsigned int> letters;
+    
+    for (unsigned int i = 0 ; i < s.size(); i++) {
 
-  return max_res;
+        char c = s[i];
+        
+        if (letters.find(c) == letters.end() ) {
+            res +=c;
+        
+            if (res.size() > max ) {
+                max_res = res;
+                max = res.size();
+            }
+
+        } else {
+            res = s.substr(letters[c]+1,i-1);
+        }
+        
+        letters[c] = i;
+    }
+
+    return max_res;
 }
