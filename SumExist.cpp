8d15ee71acf10c4f4eea7e8cddce7892397f79e3
amir <am5050@gmail.com>
2018-11-30 16:39:23 +0200

    .

---
+++ SumExist.cpp
@@ -0,0 +1,47 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+#include <string>
+
+#define ABS(x) (x > 0 ? x : -1 * x )
+
+using namespace std;
+
+bool isSumExist(int* arr, int n, int sum)
+{
+    if( sum == 0) 
+        return true;
+    
+    if(n == 0)
+        return false;
+
+    return isSumExist(arr +1,  n-1, sum - *arr) ||
+           isSumExist(arr +1,  n-1, sum );
+}
+
+int main(int argc, char * argv[])
+{
+    srand(time(NULL));
+
+    cout << sqrt2(9) << endl;
+    cout << sqrt2(2) << endl;
+    cout << sqrt2(2.5) << endl;
+
+
+    cout << sqrt3(27) << endl;
+    cout << sqrt3(9) << endl;
+    cout << sqrt3(10.5) << endl;
+    
+    return 0;
+}
+
+
+
+
+
